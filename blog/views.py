2e9bb213ae7bfb7f0118659619c34ea3e8005181
xinxin <xinxin@rrkd.cn>
2016-06-14 11:07:33 +0800

    1,添加发帖登陆验证 2,修改post_edit页面的 按钮类型错误 3,views.py添加新的引入文件 4,为项目添加工具 debug_toolbar

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import render,get_object_or_404
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
@@ -24,21 +25,21 @@ def post_new(request):
             post.author = request.user
             post.published_date = datetime.now()
             post.save()
-            return redirect('blog.views.post_detail',pk=post.pk)
+            return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request,'blog/post_edit.html',{'form':form})
+    return render(request, 'blog/post_edit.html', {'form': form})
 
-def post_edit(request,pk):
-    post = get_object_or_404(Post,pk=pk)
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST,instance=post)
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = datetime.now()
             post.save()
-            return redirect('blog.views.post_detail',pk=post.pk)
+            return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request,'blog/post_edit.html',{'form':form})
+    return render(request, 'blog/post_edit.html', {'form': form})
