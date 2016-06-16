65c295a6dbaae3877676140b0a40cdda489b5a0f
xinxin <xinxin@rrkd.cn>
2016-06-16 17:50:57 +0800

    1,生成评论表 (https://github.com/DjangoGirls/tutorial-extensions/blob/master/homework_create_more_models/README.md) 2,添加评论模块

---
+++ blog/migrations/0002_auto_20160616_1535.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('author', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('created_date', models.DateTimeField(default=datetime.datetime.now)),
+                ('approved_comment', models.BooleanField(default=False)),
+            ],
+        ),
+        migrations.AlterField(
+            model_name='post',
+            name='created_date',
+            field=models.DateTimeField(default=datetime.datetime.now),
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='post',
+            field=models.ForeignKey(related_name='comments', to='blog.Post'),
+        ),
+    ]
